554ec141a50e60728928c7a62a4a46a91779fb20
anjali <anjali@onpagechamp.com>
2022-02-18 19:15:40 +0530

    test

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,9 +3,11 @@ import { Tabs, Tab } from '@material-ui/core';
 import TopNav from '../../components/TopNavBar/TopNav';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import '../../css/common.css';
+import { connect } from 'react-redux';
+import { getAds } from '../../../actions/admin/ads/AdsActions';
 
-const Dashboard = () => {
-  const [tabIndex, setTabIndex] = useState(0);
+const Dashboard = ({ dispatch }) => {
+  
 
   return (
       <div className="DashboardPage">
@@ -24,31 +26,31 @@ const Dashboard = () => {
          <div id="content-page" className="content-page">
          <div class="row">
             <div class="col-sm-4">
-              <div class="card">
+              {/* <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Special title treatment</h5>
                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
-              </div>
+              </div> */}
             </div>
             <div class="col-sm-4">
-              <div class="card">
+              {/* <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Special title treatment</h5>
                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
-              </div>
+              </div> */}
             </div>
             <div class="col-sm-4">
-              <div class="card">
+              {/* <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Special title treatment</h5>
                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
          {/* <div className="container">
@@ -7939,4 +7941,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default connect()(Dashboard);
